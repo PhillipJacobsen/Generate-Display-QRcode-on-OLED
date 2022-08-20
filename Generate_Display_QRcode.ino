9aa76732bc7715f753057a1d8bd1eb829460f3c4
Developer-39 <111531289+Developer-39@users.noreply.github.com>
2022-08-20 16:33:02 -0400

    Fixed Spacing

--- Generate_Display_QRcode.ino
+++ Generate_Display_QRcode.ino
@@ -44,18 +44,23 @@
 
 ============ License for the QRCode library ============
 The MIT License (MIT)
+
 This library is written and maintained by Richard Moore.
 Major parts were derived from Project Nayuki's library.
+
 Copyright (c) 2017 Richard Moore     (https://github.com/ricmoo/QRCode)
 Copyright (c) 2017 Project Nayuki    (https://www.nayuki.io/page/qr-code-generator-library)
+
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
+
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
+
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -92,16 +97,20 @@ QRCode qrcode;                  // Create the QR code
 
 ============ BSD License for U8g2lib Code ============
 Universal 8bit Graphics Library (https://github.com/olikraus/u8g2)
+
 Copyright (c) 2016, olikraus@gmail.com
 All rights reserved.
+
 Redistribution and use in source and binary forms, with or without modification, 
 are permitted provided that the following conditions are met:
+
 * Redistributions of source code must retain the above copyright notice, this list 
   of conditions and the following disclaimer.
-  
+
 * Redistributions in binary form must reproduce the above copyright notice, this 
   list of conditions and the following disclaimer in the documentation and/or other 
   materials provided with the distribution.
+
 THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
 CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
 INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
